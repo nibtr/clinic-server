ac869cf36b6f425bb8de1f3e052f55a9e6dc9bc7
nhman2002 <nhman2002@gmail.com>
2023-07-29 18:03:12 +0700

    loggin & autho

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,39 +1,61 @@
-import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken'
 import { comparePassword } from '../utils/passwordUtil'
 import { Request, Response } from 'express';
 import prismaClient from "../utils/prismaClient";
+import config from '../routes/configs';
 
 
 export const login = async (req: Request, res: Response) => {
-    const {username, password} = req.body;
-    try{
-        if (!{username, password}) {
+    const { username, password } = req.body;
+    try {
+        if (!{ username, password }) {
             res.status(400).json({ error: 'Username is required' });
             return;
         }
-        await prismaClient.$transaction(async (tx)=>{
+        await prismaClient.$transaction(async (tx) => {
             const account = await tx.account.findUnique({
-                where:{
+                where: {
                     username: String(username)
+                },
+                select: {
+                    id: true,
+                    username: true,
+                    password: true,
+                    Personnel: {
+                        select: {
+                            type: true
+                        }
+                    }
                 }
             })
-            if(!account){
-                res.status(200).json({
-                    username, password
-                })
+            if (!account) {
+                res.status(401).json({
+                    error: "Error",
+                    message: "Account not found"
+                });
+                return
             }
-        })
-
-        
-        res.status(200).json({
-            username, password
+            const storedPassword = account.password
+            const isMatch = await comparePassword(req.body.password, storedPassword)
+            if (!isMatch) {
+                res.status(401).json({
+                    error: "Error",
+                    message: "Invalid username or password"
+                });
+                return
+            }
+            const token = jwt.sign({ id: account.id }, String(config.jwtToken))
+            const returnObject = {
+                token,
+                username: account.username,
+                type: account.Personnel.type
+            }
+            res.status(200).json({
+                returnObject
+            })
         })
     }
-    catch(error){
+    catch (error) {
 
     }
-
-
-    
 }
