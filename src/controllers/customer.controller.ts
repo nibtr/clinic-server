fc0b15ae3373ff448fdba5c2cbbb7178f2bba458
Trung4123 <thieuvinhtrung4123@gmail.com>
2023-07-31 22:19:55 +0700

    Update customer.controller.ts

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -7,35 +7,37 @@ export const makeAppointment = async (
     response: Response,
     next: NextFunction
 ) => {
-
-    let { name, phone, appointmentTime, requestTime, note } = request.body;
-    const duplicateApm = await prismaClient.appointmentRequest.find({
-        where: {
-            phone: phone,
-            appointmentTime: appointmentTime,
-        }
-    });
-
-    if (phone.length !== 10) {
-        response.status(400).json({ message: "Phone number is invalid" });
-        return;
-    }
-
-    if (duplicateApm) {
-        response.status(400).json({ message: "Appointment request is existed" });
-        return;
-    }
-
     try {
-        const appointment = await prismaClient.appointmentRequest.create({
-            data: {
-                name: name,
+        const { name, phone, appointmentTime, requestTime, note } = request.body;
+        const duplicateApm = await prismaClient.appointmentRequest.findFirst({
+            where: {
                 phone: phone,
-                appointmentTime: new Date(appointmentTime),
-                requestTime: new Date(requestTime),
-                note: note,
+                appointmentTime: appointmentTime,
             }
         });
+
+        if (duplicateApm) {
+            response.status(400).json({ message: "Appointment request is existed" });
+            return;
+        }
+
+        if (phone.length !== 10) {
+            response.status(400).json({ message: "Phone number is invalid" });
+            return;
+        }
+
+        const appointment = await prismaClient.$transaction([
+            prismaClient.appointmentRequest.create({
+                data: {
+                    name: name,
+                    phone: phone,
+                    appointmentTime: appointmentTime,
+                    requestTime: requestTime,
+                    note: note,
+                }
+
+            }),
+        ]);
 
         return response.status(200).json(
             messageResponse(200, appointment)
