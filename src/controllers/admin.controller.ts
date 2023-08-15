034f4e523505bf18bd3dd3746a608a28ee1d8a8d
nhman2002 <nhman2002@gmail.com>
2023-08-15 16:22:17 +0700

    WIP

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,10 +2,18 @@ import { STAFF_TYPE } from "../constant";
 import { getPersonnelFollowingType } from "./common.controller";
 import { messageResponse } from "../utils/messageResponse";
 import { NextFunction, Request, Response } from "express";
+import * as utils from "../utils/passwordUtil"
+import prismaClient from "../utils/prismaClient";
 
 export const getStaffs = getPersonnelFollowingType(STAFF_TYPE);
 
 export const createStaff = async (
     req: Request,
     res: Response,
-)
+    next: NextFunction
+) => {
+    try{
+        const hashPassword = await utils.hashPassword(req.body.password)
+        await prismaClient.$transaction
+    }
+}
